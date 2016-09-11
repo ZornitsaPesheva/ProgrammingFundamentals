ee78430912189ede93c5ebdf5a4cfe33d96a2b39
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-11 10:00:32 +0300

    Debugging

---
+++ Debugging - Exercises/02. Be Positive/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BePositive_broken
+{
+	public static void Main()
+	{
+		int countSequences = int.Parse(Console.ReadLine());
+        //четене се всчики редове
+		for (int i = 0; i < countSequences; i++)
+		{
+            //четене на един ред
+            string[] input = Console.ReadLine().Trim().Split(' ');
+			var numbers = new List<int>();
+            //махане на интервалите
+			for (int j = 0; j < input.Length; j++)
+			{
+				if (!input[j].Equals(string.Empty))
+				{
+					int num = int.Parse(input[j]);
+					numbers.Add(num);
+				}
+			}
+            // намерено е число
+            bool found = false;
+
+            for (int j = 0; j < numbers.Count; j++)
+			{
+                
+                int currentNum = numbers[j];
+
+				if (currentNum >= 0)
+				{
+					if (found)
+						{
+							Console.Write(" ");
+						}
+
+						Console.Write(currentNum);
+
+						found = true;
+				}
+				else
+				{
+                    if (j < numbers.Count - 1)
+                    {
+                        currentNum += numbers[j + 1];
+                    }
+                    j++;
+					if (currentNum >= 0)
+					{
+						if (found)
+						{
+							Console.Write(" ");
+						}
+
+						Console.Write(currentNum);
+
+						found = true;
+					}
+				}
+			}
+
+			if (!found)
+			{
+				Console.WriteLine("(empty)");
+			}
+            else
+            {
+                Console.WriteLine();
+            }
+		}
+	}
+}
---
+++ Debugging - Exercises/4. Debugging Exercise - Instruction Set/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class InstructionSet_broken
+{
+    static void Main()
+    {
+        string opCode = "";
+
+        while (opCode != "END")
+        {
+            opCode = Console.ReadLine();
+            string[] codeArgs = opCode.Split(' ');
+
+            long result = 0;
+            switch (codeArgs[0])
+            {
+                case "INC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        operandOne++;
+                        result = operandOne;
+                        break;
+                    }
+                case "DEC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        operandOne = operandOne - 1;
+                        result = operandOne;
+                        break;
+                    }
+                case "ADD":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result = operandOne + operandTwo;
+                        break;
+                    }
+                case "MLA":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result = operandOne * operandTwo;
+                        break;
+                    }
+            }
+            if (result != 0)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
