328386462dad9a73e18051b0583658cbdafdaae8
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-17 18:19:54 +0300

    DataTypesAndMethods

---
+++ DataTypesAndMethods/4. Elevator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            double courses = Math.Ceiling((double)n / (double)p);
+            Console.WriteLine(courses);
+        }
+    }
+}
