44c8735fbeb836151464b9586f460987ccd184a6
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-17 10:05:29 +0300

    DateTypes

---
+++ DataTypesAndMethods/3. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
