be43e963abd7fb4c1fe8f9329bb555b79c63f57b
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-18 09:02:10 +0300

    DaataTypes and methods

--- Data Types and Methods - Exercises/4. Variable in Hexadecimal Format/Program.cs
+++ Data Types and Methods - Exercises/4. Variable in Hexadecimal Format/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _4.Variable_in_Hexadecimal_Format
 {
@@ -10,6 +6,8 @@ namespace _4.Variable_in_Hexadecimal_Format
     {
         static void Main(string[] args)
         {
+            string hex = Console.ReadLine();
+            Console.WriteLine(Convert.ToInt32(hex, 16));
         }
     }
 }
