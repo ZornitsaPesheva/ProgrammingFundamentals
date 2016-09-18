c8e38e23f6232ed70ac8be9645ee1fe68ea8bb02
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-18 15:59:07 +0300

    Data Types and Variables - Exercises

---
+++ Data Types and Methods - Exercises/7. Exchange Variable Values/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Exchange_Variable_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("Before:");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            int x = b;
+            b = a;
+            a = x;
+            Console.WriteLine("After:");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+        }
+    }
+}
