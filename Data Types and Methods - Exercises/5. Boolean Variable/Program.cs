0ff51724bc980dbeb81a3fbca8c0d75bbf31c43b
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-18 14:08:54 +0300

    Data Types and Variables - Exercises

---
+++ Data Types and Methods - Exercises/5. Boolean Variable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5.Boolean_Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string boolean = Console.ReadLine();
+            if (Convert.ToBoolean(boolean))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ Data Types and Methods - Exercises/6. Strings and Objects/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _6.Strings_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello";
+            string s2 = "World";
+            object s3 = s1 + " " + s2;
+            string str = (string)s3;
+            Console.WriteLine(str);
+        }
+    }
+}
