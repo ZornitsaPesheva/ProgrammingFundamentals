cc05dca7b87968edd474af8a4f8f2036d645e277
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-19 20:31:28 +0300

    DataTypesAndMethods

---
+++ Data Types and Methods - Exercises/09. Reversed chars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09.Reversed_chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+            Console.Write(c);
+            Console.Write(b);
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Data Types and Methods - Exercises/8. Employee Data/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _8.Employee_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Amanda";
+            string lastName = "Jonson";
+            short age = 27;
+            char gender = 'f';
+            long personalId = 8306112507;
+            uint number = 27563571;
+            Console.WriteLine($"First name: {firstName}");
+            Console.WriteLine($"Last name: {lastName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Gender: {gender}");
+            Console.WriteLine($"Personal ID: {personalId}");
+            Console.WriteLine($"Unique Employee number: {number}");
+        }
+    }
+}
