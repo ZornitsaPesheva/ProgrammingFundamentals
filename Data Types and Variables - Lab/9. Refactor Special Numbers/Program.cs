10b7d24f070ddbba0c832d57578ac567b487c4da
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-22 17:02:30 +0300

    Data Types and Variables - Lab

---
+++ Data Types and Variables - Lab/9. Refactor Special Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _9.Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                int num = i;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                bool special = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{i} -> {special}");
+            }
+        }
+    }
+}
