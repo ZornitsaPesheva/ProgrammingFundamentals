63f01f5fe0cd965a6cb1056930b2ae170daea403
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-22 18:31:00 +0300

    Data Types and Variables - Exercises

--- Data Types and Variables - Exercises/10. Centuries to Nanoseconds/Program.cs
+++ Data Types and Variables - Exercises/10. Centuries to Nanoseconds/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _10.Centuries_to_Nanoseconds
 {
@@ -10,6 +6,24 @@ namespace _10.Centuries_to_Nanoseconds
     {
         static void Main(string[] args)
         {
+            int centuries = int.Parse(Console.ReadLine());
+            Console.Write($"{centuries} centuries = ");
+            int years = centuries * 100;
+            Console.Write($"{years} years = ");
+            int days =(int)(years * 365.2422);
+            Console.Write($"{days} days = ");
+            int hours = days * 24;
+            Console.Write($"{hours} hours = ");
+            long minutes = hours * 60;
+            Console.Write($"{minutes} minutes = ");
+            ulong seconds = (ulong)minutes * 60;
+            Console.Write($"{seconds} seconds = ");
+            ulong milliseconds = seconds * 1000;
+            Console.Write($"{milliseconds} milliseconds = ");
+            decimal microseconds = milliseconds * 1000;
+            Console.Write($"{microseconds} microseconds = ");
+            decimal nanoseconds = microseconds * 1000;
+            Console.WriteLine($"{nanoseconds} nanoseconds");
         }
     }
 }
