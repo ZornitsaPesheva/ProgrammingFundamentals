59a5389bfeacdb648fb26443118e6d953cf269fb
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-22 16:30:37 +0300

    Data Types and Variables

--- Data Types and Variables - Lab/8. Refactor Volume of Pyramid/Program.cs
+++ Data Types and Variables - Lab/8. Refactor Volume of Pyramid/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _8.Refactor_Volume_of_Pyramid
 {
@@ -10,15 +6,14 @@ namespace _8.Refactor_Volume_of_Pyramid
     {
         static void Main(string[] args)
         {
-            double dul, sh, V = 0;
             Console.Write("Length: ");
-            dul = double.Parse(Console.ReadLine());
+            double length = double.Parse(Console.ReadLine());
             Console.Write("Width: ");
-            sh = double.Parse(Console.ReadLine());
-            Console.Write("Heigth: ");
-            V = double.Parse(Console.ReadLine());
-            V = (dul + sh + V) / 3;
-            Console.WriteLine("Pyramid Volume: {0:F2}", V);
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double height = double.Parse(Console.ReadLine());
+            var volume = (length * width * height) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", volume);
         }
     }
 }
