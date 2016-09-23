1187ba26ee13f4e6815caae43314d60146a73fa8
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-23 13:48:36 +0300

    Data types and Variables

---
+++ Data Types and Variables - Exercises/19. Thea the Photographer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _19.Thea_the_Photographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int ft = int.Parse(Console.ReadLine());
+            int ff = int.Parse(Console.ReadLine());
+            int ut = int.Parse(Console.ReadLine());
+            long allft = n * (long)ft;
+            double percentige = ff / 100.0;
+            int goodForUpload = (int)Math.Ceiling(n * percentige);
+            long seconds = goodForUpload * (long)ut + allft;
+            Console.WriteLine(TimeSpan.FromSeconds(seconds).ToString(new string('d', 1) + @"\:hh\:mm\:ss"));
+        }
+    }
+}
