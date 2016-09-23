1cfe86fcb0b64e9253fb3540b16c7586311fd07e
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-23 09:44:01 +0300

    Data Types and Variables

---
+++ Data Types and Variables - Exercises/16. Comparing Floats/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _16.Comparing_Floats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+           // double eps = 0.000001;
+            Console.WriteLine(Math.Abs((int)(n1 * 1000000) - (int)(n2 * 1000000)) < 1);
+        }
+    }
+}
