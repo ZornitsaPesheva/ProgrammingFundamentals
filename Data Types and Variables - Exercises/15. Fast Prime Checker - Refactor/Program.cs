77daa7f04a0fe4ade52f6efd7e66ab107d8d04b6
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-23 09:28:28 +0300

    Data Types and Variables

---
+++ Data Types and Variables - Exercises/15. Fast Prime Checker - Refactor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _15.Fast_Prime_Checker___Refactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                bool prime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {prime}");
+            }
+        }
+    }
+}
