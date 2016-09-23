64d0d99658536a52054eea26ba9e47647c14ea79
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-23 10:21:31 +0300

    Data Types and Variables

---
+++ Data Types and Variables - Exercises/17. Print Part of the ASCII Table/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _17.Print_Part_of_the_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int stop = int.Parse(Console.ReadLine());
+            for (int i = start; i <= stop; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
