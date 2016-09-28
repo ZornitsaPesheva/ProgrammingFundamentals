10c1184a1fe3a3319ae6c97eb06e0e3062679fc7
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-09-28 16:46:09 +0300

    Methods Debugging Troubleshooting

---
+++ MethodsDebugginTroublesh/02. Sign of IntegerNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Sign_of_IntegerNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintSign(n);
+        }
+
+        static void PrintSign(int n)
+        {
+            Console.Write("The number ");
+            if (n > 0)
+            {
+                Console.WriteLine($"{n} is positive.");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"{n} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"{n} is zero.");
+            }
+        }
+    }
+}
---
+++ MethodsDebugginTroublesh/1. Blank Receipt/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1.Blank_Receipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintReceipt();
+        }
+
+        static void PrintReceiptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+
+        static void PrintReceiptBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+
+        static void PrintReceiptFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\u00A9 SoftUni");
+        }
+
+        static void PrintReceipt()
+        {
+            PrintReceiptHeader();
+            PrintReceiptBody();
+            PrintReceiptFooter();
+        }
+
+
+    }
+}
