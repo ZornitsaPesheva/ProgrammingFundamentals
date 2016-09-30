11cb86c1ba9b6b4f53b00282c7e9babb3625d025
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-09-30 12:55:13 +0300

    Methods and Debugging

---
+++ MethodsAndDebuggingExerc/04. Numbers in Reversed Order/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _04.Numbers_in_Reversed_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal n = decimal.Parse(Console.ReadLine());
+            decimal reversed = ReverseN(n);
+            Console.WriteLine(reversed);
+        }
+
+        private static decimal ReverseN(decimal n)
+        {
+            string str = n.ToString();
+            string rstr = ReverseS(str);
+            return decimal.Parse(rstr);
+
+        }
+
+        private static string ReverseS(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
