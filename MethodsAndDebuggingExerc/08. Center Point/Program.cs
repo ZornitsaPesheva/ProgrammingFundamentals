2a1cb083c3871ef72ca6cb993b342c07ccd71dbe
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-09-30 20:07:27 +0300

    Methods and Debugging

--- MethodsAndDebuggingExerc/08. Center Point/Program.cs
+++ MethodsAndDebuggingExerc/08. Center Point/Program.cs
@@ -23,6 +23,7 @@ namespace _8.Center_Point
                 Console.WriteLine($"({x1}, {y1})");
             }
         }
+
         static double GetDistance(double x1, double y1, double x2, double y2)
         {
             double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
--- MethodsAndDebuggingExerc/09. Longer Line/Program.cs
+++ MethodsAndDebuggingExerc/09. Longer Line/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _09.Longer_Line
 {
@@ -10,6 +6,49 @@ namespace _09.Longer_Line
     {
         static void Main(string[] args)
         {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+            double line1 = GetDistance(x2, y2, x1, y1);
+            double line2 = GetDistance(x4, y4, x3, y3);
+
+            if (line1 >= line2)
+            {
+                double x1y1ToCenter = GetDistance(x1, y1, 0, 0);
+                double x2y2ToCenter = GetDistance(x2, y2, 0, 0);
+                if (x1y1ToCenter <= x2y2ToCenter)
+                {
+                    Console.WriteLine($"({x1}, {y1})({x2}, {y2})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x2}, {y2})({x1}, {y1})");
+                }
+            }
+            else
+            {
+                double x3y3ToCenter = GetDistance(x3, y3, 0, 0);
+                double x4y4ToCenter = GetDistance(x4, y4, 0, 0);
+                if (x3y3ToCenter <= x4y4ToCenter)
+                {
+                    Console.WriteLine($"({x3}, {y3})({x4}, {y4})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x4}, {y4})({x3}, {y3})");
+                }
+            }
+        }
+
+        static double GetDistance(double x1, double y1, double x2, double y2)
+        {
+            double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return distance;
         }
     }
 }
