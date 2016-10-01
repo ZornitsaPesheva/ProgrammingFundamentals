2be3e6efc7f7dc6d564e12fd550fc9996384cb0b
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-01 18:06:39 +0300

    Methods and Debugging

---
+++ MethodsAndDebuggingExerc/15. Instruction Set/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class InstructionSet
+{
+    static void Main()
+    {
+        
+        long result = 0;
+        string opCode = "";
+
+        while (opCode != "END")
+        {
+            opCode = Console.ReadLine();
+            string[] codeArgs = opCode.Split(' ');
+            
+            switch (codeArgs[0])
+            {
+                case "INC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        result = ++operandOne;
+                        break;
+                    }
+                case "DEC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        result = --operandOne;
+                        break;
+                    }
+                case "ADD":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result = operandOne + operandTwo;
+                        break;
+                    }
+                case "MLA":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result = operandOne * operandTwo;
+                        break;
+                    }
+             }
+            if (opCode != "END")
+            {
+                Console.WriteLine(result);
+            }
+        } 
+    }
+}
