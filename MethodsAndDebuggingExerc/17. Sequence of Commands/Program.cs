19960250ac6525be735c7f46ea81f43ceb0ebe44
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-01 18:19:16 +0300

    Methods and Debugging

---
+++ MethodsAndDebuggingExerc/17. Sequence of Commands/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+
+public class SequenceOfCommands
+{
+    private const char ArgumentsDelimiter = ' ';
+
+    public static void Main()
+    {
+        int sizeOfArray = int.Parse(Console.ReadLine());
+
+        long[] array = Console.ReadLine()
+            .Split(ArgumentsDelimiter)
+            .Select(long.Parse)
+            .ToArray();
+
+        //  string command = Console.ReadLine();
+        string command = "";
+        while (!command.Equals("stop"))
+        {
+            string[] line = Console.ReadLine().Trim().Split(' ').ToArray();
+            command = line[0];
+            if (command.Equals("stop"))
+            {
+                break;
+            }
+            //      string line = Console.ReadLine().Trim();
+            int[] args = new int[2];
+
+            if (command.Equals("add") ||
+                command.Equals("subtract") ||
+                command.Equals("multiply"))
+            {
+                //  string[] stringParams = line.Split(ArgumentsDelimiter);
+                args[0] = int.Parse(line[1]);
+                args[1] = int.Parse(line[2]);
+
+                //   PerformAction(array, command, args);
+            }
+
+            PerformAction(array, command, args);
+
+            PrintArray(array);
+            Console.WriteLine();
+
+            // command = Console.ReadLine();
+        }
+    }
+
+    static void PerformAction(long[] arr, string action, int[] args)
+    {
+        //   long[] array = arr.Clone() as long[];
+        int pos = args[0];
+        int value = args[1];
+
+        switch (action)
+        {
+            case "multiply":
+                arr[pos - 1] *= value;
+                break;
+            case "add":
+                arr[pos - 1] += value;
+                break;
+            case "subtract":
+                arr[pos - 1] -= value;
+                break;
+            case "lshift":
+                ArrayShiftLeft(arr);
+                break;
+            case "rshift":
+                ArrayShiftRight(arr);
+                break;
+        }
+    }
+
+    private static void ArrayShiftRight(long[] array)
+    {
+
+        var x = array[array.Length - 1];
+        for (int i = array.Length - 1; i >= 1; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[0] = x;
+    }
+
+    private static void ArrayShiftLeft(long[] array)
+    {
+        var x = array[0];
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        array[array.Length - 1] = x;
+    }
+
+    private static void PrintArray(long[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
---
+++ MethodsAndDebuggingExerc/18. Substring/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Substring_broken
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        int jump = int.Parse(Console.ReadLine());
+
+        //  const char Search = 'р';
+        bool hasMatch = false;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == 'p')
+            {
+
+                hasMatch = true;
+
+                int letters = jump + 1;
+
+                if (letters >= text.Length - i)
+                {
+                    letters = text.Length - i;
+                }
+
+                string matchedString = text.Substring(i, letters);
+                Console.WriteLine(matchedString);
+                i += jump;
+            }
+
+        }
+
+        if (!hasMatch)
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
