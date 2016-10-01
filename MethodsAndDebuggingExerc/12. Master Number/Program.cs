de65a3e77927754498b4a0d21e4b20a9808b320f
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-01 16:11:18 +0300

    Methods and Debugging

---
+++ MethodsAndDebuggingExerc/12. Master Number/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _12.Master_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                if (IsPalindrome(i) && SumOfDigitsIs7(i) && ContainEvenDigit(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static bool IsPalindrome(int i)
+        {
+            int n = i;
+            int rev = 0;
+            while (i > 0)
+            {
+                int dig = i % 10;
+                rev = rev * 10 + dig;
+                i = i / 10;
+            }
+            if (n == rev)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool SumOfDigitsIs7(int i)
+        {
+            int sum = 0;
+            while (i > 0)
+            {
+                sum += i % 10;
+                i = i / 10;
+            }
+            if (sum % 7 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool ContainEvenDigit(int i)
+        {
+            while (i > 0)
+            {
+                if (i % 10 % 2 == 0)
+                {
+                    return true;
+                }
+                i /= 10;
+            }
+            return false;
+        }
+    }
+}
