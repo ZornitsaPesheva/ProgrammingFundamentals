51f7e6f26e0b4c58e38a2b396a24e46516112138
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-01 16:22:42 +0300

    Methods and Debugging

---
+++ MethodsAndDebuggingExerc/13. Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace _13.Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(FactorialOfN(n));
+        }
+
+        private static BigInteger FactorialOfN(int n)
+        {
+            BigInteger factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+}
+}
