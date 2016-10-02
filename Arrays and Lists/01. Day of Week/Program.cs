886747e6c232125b22e5a8d2e60079cedca9d449
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-02 14:42:00 +0300

    Arrays

---
+++ Arrays and Lists/01. Day of Week/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int day = int.Parse(Console.ReadLine());
+            if (day > 0 && day < 8)
+            {
+                Console.WriteLine(arr[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Day!");
+            }
+        }
+    }
+}
---
+++ Arrays and Lists/02. Reverse Array of Integers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Reverse_Array_of_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            arr = arr.Reverse().ToArray();
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
