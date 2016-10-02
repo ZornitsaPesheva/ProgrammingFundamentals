c8fb371535d10f60ba99d91e1e80b84ea6f7709d
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-02 17:50:03 +0300

    Arrays and Lists

--- Arrays and Lists/04. Tripple Sum/Program.cs
+++ Arrays and Lists/04. Tripple Sum/Program.cs
@@ -10,6 +10,25 @@ namespace _04.Tripple_Sum
     {
         static void Main(string[] args)
         {
+            int[] arr = Console.ReadLine().Split(' ')
+                .Select(int.Parse).ToArray();
+            int count = 0;
+            for (int a = 0; a < arr.Length - 1; a++)
+            {
+                for (int b = a + 1; b < arr.Length; b++)
+                {
+                    int sum = arr[a] + arr[b];
+                    if (arr.Contains(sum))
+                    {
+                        Console.WriteLine($"{arr[a]} + {arr[b]} = {sum}");
+                        count++;
+                    }
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
