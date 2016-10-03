aea96bd80b8955023ce659611dfe6e96bdce616e
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-03 10:12:04 +0300

    Arrays and Lists

---
+++ Arrays and Lists/07. Sum Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Sum_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split(' ').
+                Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split(' ').
+                Select(int.Parse).ToArray();
+
+            int len = Math.Max(arr1.Length, arr2.Length);
+
+            int[] result = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                result[i] =
+                    arr1[i % arr1.Length] +
+                    arr2[i % arr2.Length];
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
