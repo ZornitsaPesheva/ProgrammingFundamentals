6c0485b935f55a2496267b20a9063bf8e0816192
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-03 23:29:35 +0300

    Arrays and Lists

--- Arrays and Lists/12. Sum Adjacent Equal Numbers/Program.cs
+++ Arrays and Lists/12. Sum Adjacent Equal Numbers/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _12.Sum_Adjacent_Equal_Numbers
 {
@@ -10,6 +8,23 @@ namespace _12.Sum_Adjacent_Equal_Numbers
     {
         static void Main(string[] args)
         {
+            List<decimal> nums =
+                Console.ReadLine().Split(' ')
+                .Select(decimal.Parse).ToList();
+            for (int i = 1; i < nums.Count; i++)
+            {
+                if (nums[i - 1] == nums[i])
+                {
+                    nums[i - 1] += nums[i];
+                    nums.RemoveAt(i);
+                    i = i - 2;
+                    if (i < 0)
+                    {
+                        i = 0;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", nums));
         }
     }
 }
