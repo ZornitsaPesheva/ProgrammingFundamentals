cfd9ee80362dbd8030e6694dbb2e1dc72a7e801c
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-03 23:12:28 +0300

    Arrays and Lists

--- Arrays and Lists/11. Append Lists/Program.cs
+++ Arrays and Lists/11. Append Lists/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _11.Append_Lists
 {
@@ -10,6 +8,23 @@ namespace _11.Append_Lists
     {
         static void Main(string[] args)
         {
+            List<string> items =
+                Console.ReadLine().Split('|').ToList();
+            items.Reverse();
+            List<string> result = new List<string>();
+
+            foreach (string item in items)
+            {
+                List<string> nums = item.Split(' ').ToList();
+                foreach (string num in nums)
+                {
+                    if (num != "")
+                    {
+                        result.Add(num);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
---
+++ Arrays and Lists/12. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Sum_Adjacent_Equal_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
