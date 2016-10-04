eae5dca0957df97b5a2d215ee60e43965f268af3
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-04 14:13:12 +0300

    Arrays and Lists

--- Arrays and Lists/13. Split by Word Casing/Program.cs
+++ Arrays and Lists/13. Split by Word Casing/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _13.Split_by_Word_Casing
 {
@@ -10,6 +7,66 @@ namespace _13.Split_by_Word_Casing
     {
         static void Main(string[] args)
         {
+            char[] separators = new char[]
+            {
+                ',', ';', ':', '.', '!', '(', ')', '"','\'', '\\', '/', '[',
+                ']', ' '
+            };
+            string[] words = Console.ReadLine().Split(separators,
+                StringSplitOptions.RemoveEmptyEntries);
+            List<string> lowerCaseList = new List<string>();
+            List<string> upperCaseList = new List<string>();
+            List<string> mixedCaseList = new List<string>();
+
+            foreach (string word in words)
+            {
+                WordType type = GetWordType(word);
+                if (type == WordType.UpperCase)
+                {
+                    upperCaseList.Add(word);
+                }
+                else if (type == WordType.lowerCase)
+                {
+                    lowerCaseList.Add(word);
+                }
+                else
+                {
+                    mixedCaseList.Add(word); 
+                }
+            }
+
+            Console.WriteLine($"Lower-case: {string.Join(", ", lowerCaseList)}");
+            Console.WriteLine($"Mixed-case: {string.Join(", ", mixedCaseList)}");
+            Console.WriteLine($"Upper-case: {string.Join(", ", upperCaseList)}");
         }
+
+        static WordType GetWordType(string word)
+        {
+            int lowerLetters = 0;
+            int upperLetters = 0;
+            foreach (char w in word)
+            {
+                if (char.IsUpper(w))
+                {
+                    upperLetters++;
+                }
+                else if (char.IsLower(w))
+                {
+                    lowerLetters++;
+                }
+            }
+            if (upperLetters == word.Length)
+            {
+                return WordType.UpperCase;
+            }
+            if (lowerLetters == word.Length)
+            {
+                return WordType.lowerCase;
+            }
+            return WordType.MixedCase;
+        }
+
+        enum WordType { UpperCase, MixedCase, lowerCase };
+
     }
 }
