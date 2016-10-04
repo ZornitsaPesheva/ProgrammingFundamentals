d744d1dcbb5b24df6952c8b1a2b459aa6186c3a6
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-04 14:31:37 +0300

    Arrays and Lists

--- Arrays and Lists/14. Sort Numbers/Program.cs
+++ Arrays and Lists/14. Sort Numbers/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _14.Sort_Numbers
 {
@@ -10,6 +8,10 @@ namespace _14.Sort_Numbers
     {
         static void Main(string[] args)
         {
+            List<double> nums = Console.ReadLine().Split(' ')
+                .Select(double.Parse).ToList();
+            nums.Sort();
+            Console.WriteLine(string.Join(" <= ", nums));
         }
     }
 }
