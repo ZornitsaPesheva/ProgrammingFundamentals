1cac37413924e2945cd34ae9a443f00434e3a041
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-05 20:17:41 +0300

    Arrays and Lists

---
+++ Arrays and Lists - Exercises/01. Largest Common End/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Largest_Common_End
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Console.ReadLine().
+                Split(' ').ToArray();
+            string[] array2 = Console.ReadLine().
+                Split(' ').ToArray();
+
+            int leftCommonEnd = 0;
+            int rightCommonEnd = 0;
+
+            int minArrayLength = Math.Min(array1.Length, array2.Length);
+            int maxArrayLength = Math.Max(array1.Length, array2.Length);
+            int diff = maxArrayLength - minArrayLength;
+
+            for (int i = 0; i < minArrayLength; i++)
+            {
+                if (array1[i] == array2[i])
+                {
+                    leftCommonEnd++;
+                }
+            }
+
+            int j = array1.Length - 1;
+            int k = array2.Length - 1;
+
+            while (j >= 0 && k >= 0)
+            {
+                if (array1[j] == array2[k])
+                {
+                    rightCommonEnd++;
+                }
+                j--;
+                k--;
+            }
+
+
+            Console.WriteLine(Math.Max(leftCommonEnd, rightCommonEnd));
+            
+        }
+    }
+}
---
+++ Arrays and Lists - Exercises/02. Rotate and Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Rotate_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().
+                Split(' ').Select(int.Parse).ToArray();
+
+            int k = int.Parse(Console.ReadLine());
+
+            int[] rotated = new int[array.Length];
+            int[] sum = new int[array.Length];
+
+            rotated = Rotate(array);
+            for (int i = 1; i <= k; i++)
+            {
+                Add(rotated, sum);
+                rotated = Rotate(rotated);             
+            }
+
+            Console.WriteLine(string.Join(" ", sum));
+        }
+
+        private static void Add(int[] rotated, int[] sum)
+        {
+            for (int i = 0; i < rotated.Length; i++)
+            {
+                sum[i] += rotated[i];
+            }
+        }
+
+        private static int[] Rotate(int[] array)
+        {
+            int[] rotated = new int[array.Length];
+            rotated[0] = array[array.Length - 1];
+            for (int i = 1; i < array.Length; i++)
+            {
+                rotated[i] = array[i - 1];
+            }
+            return rotated;
+        }
+    }
+}
