f1ccf81ef4c2be1c97679cc34b2a7479f5319d5f
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-07 07:49:51 +0300

    Arrays and Lists - Exercises

--- Arrays and Lists - Exercises/03. Fold and Sum/Program.cs
+++ Arrays and Lists - Exercises/03. Fold and Sum/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _03.Fold_and_Sum
 {
@@ -10,6 +7,50 @@ namespace _03.Fold_and_Sum
     {
         static void Main(string[] args)
         {
+            int[] array = Console.ReadLine().
+                Split(' ').Select(int.Parse).ToArray();
+            int k = array.Length / 4;
+            int[] firstRow = Fold(array, k);
+            int[] secondRow = CutMiddle(array, k);
+            int[] result = SumArr(firstRow, secondRow);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static int[] SumArr(int[] firstRow, int[] secondRow)
+        {
+            int[] newArray = new int[firstRow.Length];
+            for (int i = 0; i < firstRow.Length; i++)
+            {
+                newArray[i] = firstRow[i] + secondRow[i];
+            }
+            return newArray;
+        }
+
+        private static int[] CutMiddle(int[] array, int k)
+        {
+            int[] newArray = new int[k * 2];
+            for (int i = 0; i < k * 2; i++)
+            {
+                newArray[i] = array[i + k];
+            }
+            return newArray;
+        }
+
+        // взема първите и последните k в отделен масив
+        private static int[] Fold(int[] array, int k)
+        {
+            int[] newArray = new int[2 * k];
+            for (int i = 0; i < k; i++)
+            {
+                newArray[i] = array[k - i - 1];
+            }
+            int x = k;
+            for (int i = 4 * k - 1; i >= 3 * k; i--)
+            {
+                newArray[x] = array[i];
+                x++;
+            }
+            return newArray;
         }
     }
 }
---
+++ Arrays and Lists - Exercises/04. Sieve of Eratosthenes/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _04.Sieve_of_Eratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool[] primes = new bool[n+1];
+            for (int i = 0; i <= n; i++)
+            {
+                primes[i] = true;
+            }
+            primes[0] = false;
+            primes[1] = false;
+            int count = 0;
+
+            for (int p = 2; p <= n; p++)
+            {
+                if (IsPrime(p)){
+                    count++;
+                //    Console.WriteLine(p);
+                    for (int i = 2; i <= n; i++)
+                    {
+                        if (i * p <= n)
+                        {
+                            primes[i * p] = false;
+                        }
+                    }
+                }
+            }
+            int[] primesOnly = ReturnPrimes(primes, count);
+            Console.WriteLine(string.Join(" ", primesOnly));
+        }
+
+        private static int[] ReturnPrimes(bool[] primes, int count)
+        {
+            int[] newArr = new int[count];
+            int x = 0;
+            for (int i = 0; i < primes.Length; i++)
+            {
+                if (primes[i] == true)
+                {
+                    newArr[x] = i;
+                    x++;
+                }
+            }
+            return newArr;
+        }
+
+        private static bool IsPrime(int v)
+        {
+            bool prime = true;
+            if (v <= 1)
+            {
+                prime = false;
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(v); i++)
+                {
+                    if (v % i == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+            }
+            return prime;
+        }
+    }
+}
