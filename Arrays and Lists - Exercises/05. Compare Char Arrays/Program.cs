7b84f266045d90678f34973941ef3a0cc92812f7
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-07 21:23:02 +0300

    Arrays and Lists

--- Arrays and Lists - Exercises/05. Compare Char Arrays/Program.cs
+++ Arrays and Lists - Exercises/05. Compare Char Arrays/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _05.Compare_Char_Arrays
 {
@@ -10,6 +7,41 @@ namespace _05.Compare_Char_Arrays
     {
         static void Main(string[] args)
         {
+            char[] array1 = Console.ReadLine().Split(' ')
+                .Select(char.Parse).ToArray();
+            char[] array2 = Console.ReadLine().Split(' ')
+                .Select(char.Parse).ToArray();
+            //     int shorter = 0;
+
+            int min = Math.Min(array1.Length, array2.Length);   
+            for (int i = 0; i < min; i++)
+            {
+                if (array1[i] < array2[i] && i < min -1)
+                {
+                    Console.WriteLine(string.Join("", array1));
+                    Console.WriteLine(string.Join("", array2));
+                    break;
+                }
+                else if (array1[i] > array2[i] && i < min - 1)
+                {
+                    Console.WriteLine(string.Join("", array2));
+                    Console.WriteLine(string.Join("", array1));
+                    break;
+                }
+                else
+                {
+                    if (i == min - 1 && array1.Length == min)
+                    {
+                        Console.WriteLine(string.Join("", array1));
+                        Console.WriteLine(string.Join("", array2));
+                    }
+                    else if (i == min - 1 && array2.Length == min)
+                    {
+                        Console.WriteLine(string.Join("", array2));
+                        Console.WriteLine(string.Join("", array1));
+                    }
+                }
+            }      
         }
     }
 }
---
+++ Arrays and Lists - Exercises/06. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
