59651d5ebc41dd203c4a19c0389d922c8ad11b9f
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-08 08:02:08 +0300

    Arrays and Lists - Exercises

--- Arrays and Lists - Exercises/11. Equal Sums/Program.cs
+++ Arrays and Lists - Exercises/11. Equal Sums/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _11.Equal_Sums
 {
@@ -10,6 +7,46 @@ namespace _11.Equal_Sums
     {
         static void Main(string[] args)
         {
+            int[] array = Console.ReadLine().
+                Split(' ').Select(int.Parse).ToArray();
+
+            int index = 0;
+            bool found = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == 0 && array.Length == 1)
+                {
+                    index = 0;
+                    found = true;
+                }
+                else
+                {
+                    int leftSum = 0;
+                    int rightSun = 0;
+                    for (int j = 0; j < i; j++)
+                    {
+                        leftSum += array[j];
+                    }
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        rightSun += array[j];
+                    }
+                    if (leftSum == rightSun)
+                    {
+                        index = i;
+                        found = true;
+                    }
+                }
+
+            }
+            if (found || array.Length == 0)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
         }
     }
 }
