753a95b7f4abff79042529aee743f60f14a69c6d
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-08 19:41:27 +0300

    Arrays and Lists

--- Arrays and Lists - Exercises/13. Longest Increasing Subsequence/Program.cs
+++ Arrays and Lists - Exercises/13. Longest Increasing Subsequence/Program.cs
@@ -9,39 +9,48 @@ namespace _13.Longest_Increasing_Subsequence
     class Program
     {
         static void Main(string[] args)
-
-
         {
-            int[] arr = { 3, 14, 5, 12, 15, 7, 8, 9, 11, 10, 1 };
-            int[] len = new int[11];
-            int[] prev = new int[11];
+            int[] arr = Console.ReadLine().
+                Split(' ').Select(int.Parse).ToArray();
+        //    int[] arr = { 3, 14, 5, 12, 15, 7, 8, 9, 11, 10, 1 };
 
+            int[] len = new int[arr.Length];
+            int[] prev = new int[arr.Length];
+            len[0] = 1;
+            prev[0] = -1;
+
+            int maxIndex = 0;
             for (int x = 0; x < arr.Length; x++)
             {
-                len[0] = 1;
-                prev[0] = -1;
-                prev[1] = 0;
+                len[x] = 1;
                 for (int i = 0; i <= x; i++)
                 {
                     if (arr[x] > arr[i])
                     {
-                        prev[x] = i;
-                    }
+                        if (arr[x] > arr[i])
+                        {
+                            prev[x] = i;
+                        }                 
+                        len[x]++;
+                     }
+                    
                 }
                 if (prev[x] > 0)
                 {
-                    len[x] = 1 + len[prev[x]];
-                }
-                else
-                {
-                    len[x] = 1;
+                   len[x] = 1 + len[prev[x]];
+                   maxIndex = prev[x] + 1;
                 }
             }
-
-            Console.WriteLine(string.Join(" ", arr));
-            Console.WriteLine(string.Join(" ", len));
-            Console.WriteLine(string.Join(" ", prev));
-
+            List<int> result = new List<int>();
+            //int lenx = len[end];
+            int j = maxIndex;
+            while (j >= 0)
+            {
+                result.Add(arr[j]);
+                j = prev[j];
+            }
+            result.Reverse();
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
