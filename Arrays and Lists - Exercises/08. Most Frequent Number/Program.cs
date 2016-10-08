c286f85b3d9232f313bb8c24fd2cd05f61501774
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-08 07:13:27 +0300

    Arrays and Lists

--- Arrays and Lists - Exercises/08. Most Frequent Number/Program.cs
+++ Arrays and Lists - Exercises/08. Most Frequent Number/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _08.Most_Frequent_Number
 {
@@ -10,6 +7,42 @@ namespace _08.Most_Frequent_Number
     {
         static void Main(string[] args)
         {
+            int[] array = Console.ReadLine().
+                Split(' ').Select(int.Parse).ToArray();
+
+            int[] counts = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[j] == array[i])
+                    {
+                        count++;
+                    }
+                }
+                counts[i] = count;
+            }
+
+            int maxIndex = FindMaxIndex(counts);
+
+            Console.WriteLine(array[maxIndex]);
+        }
+
+        private static int FindMaxIndex(int[] array)
+        {
+            int max = array[0];
+            int maxIndex = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    maxIndex = i;
+                }            
+            }
+            return maxIndex;           
         }
     }
 }
---
+++ Arrays and Lists - Exercises/09. Index of Letters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Index_of_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
