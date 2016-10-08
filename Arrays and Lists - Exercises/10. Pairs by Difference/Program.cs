9ae57c39ea8562d3f3b4e7f00071104923b2a035
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-08 07:35:03 +0300

    Arrays and Lists - Exercises

--- Arrays and Lists - Exercises/10. Pairs by Difference/Program.cs
+++ Arrays and Lists - Exercises/10. Pairs by Difference/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _10.Pairs_by_Difference
 {
@@ -10,6 +7,23 @@ namespace _10.Pairs_by_Difference
     {
         static void Main(string[] args)
         {
+            int[] array = Console.ReadLine().
+                Split(' ').Select(int.Parse).ToArray();
+            int difference = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (Math.Abs(array[i] - array[j]) == difference)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
         }
     }
 }
