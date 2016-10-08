340142d08e2942d3fc722f0ee76d10f6281fe825
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-08 07:28:07 +0300

    Arrays ans Lists - Exercises

--- Arrays and Lists - Exercises/09. Index of Letters/Program.cs
+++ Arrays and Lists - Exercises/09. Index of Letters/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _09.Index_of_Letters
 {
@@ -10,6 +6,27 @@ namespace _09.Index_of_Letters
     {
         static void Main(string[] args)
         {
+            string word = Console.ReadLine();
+
+            char[] array = new char[26];
+
+            char currentChar = 'a';
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = currentChar;
+                currentChar++;
+            }
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (word[i] == array[j])
+                    {
+                        Console.WriteLine($"{word[i]} -> {j}");
+                    }
+                }
+            }
         }
     }
 }
