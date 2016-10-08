287f9917c95d8ef7fba83ba18907b280b71c5f0c
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-08 08:18:45 +0300

    Arrays and Lists - Exercises

--- Arrays and Lists - Exercises/12. Max Sequence of Equal Elements/Program.cs
+++ Arrays and Lists - Exercises/12. Max Sequence of Equal Elements/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _12.Max_Sequence_of_Equal_Elements
 {
@@ -10,6 +8,38 @@ namespace _12.Max_Sequence_of_Equal_Elements
     {
         static void Main(string[] args)
         {
+            List<string> numbers =
+                Console.ReadLine().Split(' ').ToList();
+
+            int currentStart = 0;
+            int currentLenght = 1;
+            int longestStart = 0;
+            int longestLenght = 1;
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentLenght++;
+                    if (currentLenght > longestLenght)
+                    {
+                        longestLenght = currentLenght;
+                        longestStart = currentStart;
+                    }
+                }
+                else
+                {
+                    currentStart = i;
+                    currentLenght = 1;
+                }
+            }
+
+            List<string> result = new List<string>();
+            for (int i = longestStart; i < longestStart + longestLenght; i++)
+            {
+                result.Add(numbers[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
