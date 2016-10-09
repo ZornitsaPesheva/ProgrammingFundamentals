9579db4f704e1cc3254f0ef4c57a068c517f5126
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-09 14:57:18 +0300

    Dictionaries, Lambda and LINQ - Lab

--- Dictionaries, Lambda and LINQ/02. Odd Occurrences/Program.cs
+++ Dictionaries, Lambda and LINQ/02. Odd Occurrences/Program.cs
@@ -10,6 +10,30 @@ namespace _02.Odd_Occurrences
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine().ToLower();
+            string[] words = input.Split(' ');
+
+            var counts = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (counts.ContainsKey(word))
+                {
+                    counts[word]++;
+                }
+                else
+                {
+                    counts[word] = 1;
+                }
+            }
+            var result = new List<string>();
+            foreach (var pair in counts)
+            {
+                if (pair.Value % 2 != 0)
+                {
+                    result.Add(pair.Key);
+                }
+            }
+            Console.WriteLine(string.Join(", ", result));
         }
     }
 }
