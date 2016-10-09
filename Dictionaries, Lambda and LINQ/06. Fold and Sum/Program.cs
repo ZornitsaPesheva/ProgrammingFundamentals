eda368b67d1b44454ed752f50bd53102bd9dabf0
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-09 17:53:19 +0300

    Dictionaries, Lambda and LINQ - Lab

--- Dictionaries, Lambda and LINQ/06. Fold and Sum/Program.cs
+++ Dictionaries, Lambda and LINQ/06. Fold and Sum/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _06.Fold_and_Sum
 {
@@ -10,6 +7,18 @@ namespace _06.Fold_and_Sum
     {
         static void Main(string[] args)
         {
+            int[] arr = Console.ReadLine()
+                .Split(' ').Select(int.Parse).ToArray();
+            int k = arr.Length / 4;
+
+            int[] row1left = arr.Take(k).Reverse().ToArray();
+            int[] row1right = arr.Reverse().Take(k).ToArray();
+            int[] row1 = row1left.Concat(row1right).ToArray();
+            int[] row2 = arr.Skip(k).Take(2 * k).ToArray();
+
+            var sumArr =
+                row1.Select((x, index) => x + row2[index]);
+            Console.WriteLine(string.Join(" ", sumArr));
         }
     }
 }
