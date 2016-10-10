6d94814aaa8b5f51c9620b029c8598484559789e
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-10 14:18:55 +0300

    Dictionaries, Lambda and LINQ - Exercises

--- DictionarieLambdaAndLINQExercises/04. Fix Emails/Program.cs
+++ DictionarieLambdaAndLINQExercises/04. Fix Emails/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _04.Fix_Emails
 {
@@ -10,6 +8,30 @@ namespace _04.Fix_Emails
     {
         static void Main(string[] args)
         {
+            var emails = new Dictionary<string, string>();
+            string entry = " ";
+            while (!entry.Equals("stop"))
+            {
+                entry = Console.ReadLine();
+                if (!entry.Equals("stop"))
+                {
+                    var name = entry;
+                    emails[name] = Console.ReadLine();
+                }
+            }
+            var edited = emails
+                .Where(e => DomainOK(e));
+            foreach (var e in edited)
+            {
+                Console.WriteLine($"{e.Key} -> {e.Value}");
+            }
+        }
+
+        private static bool DomainOK(KeyValuePair<string, string> e)
+        {
+            string[] args = e.Value.Split('.');
+            string last = args[args.Length - 1];
+            return last != "us" && last != "uk";
         }
     }
 }
