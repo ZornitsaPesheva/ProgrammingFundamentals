0849e61dd79a218ed9b5fc664f619dedaeae2af8
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-10 09:38:14 +0300

    Dictionaries, Lambda and LINQ - Exercises

---
+++ DictionarieLambdaAndLINQExercises/02. Phonebook Upgrade/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Phonebook_Upgrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phonebook = new SortedDictionary<string, string>();
+            string entry = " ";
+            var phone = new string[3];
+            while (!entry.Equals("END"))
+            {
+                entry = Console.ReadLine();
+                if (entry.Equals("ListAll"))
+                {
+                    foreach (var record in phonebook)
+                    {
+                        Console.WriteLine($"{record.Key} -> {record.Value}");
+                    }
+                }
+                else
+                {
+                    phone = entry.Split(' ').ToArray();
+                    if (phone[0] == "A")
+                    {
+                        phonebook[phone[1]] = phone[2];
+                    }
+                    else if (phone[0] == "S")
+                    {
+                        if (phonebook.ContainsKey(phone[1]))
+                        {
+                            Console.WriteLine($"{phone[1]} -> {phonebook[phone[1]]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Contact {phone[1]} does not exist.");
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
