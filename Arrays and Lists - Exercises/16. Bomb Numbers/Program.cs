6dab62713e6d4f7be2cf75324a07f4044ad89c7a
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-11 06:42:31 +0300

    Arrays and Lists - Exercises

--- Arrays and Lists - Exercises/16. Bomb Numbers/Program.cs
+++ Arrays and Lists - Exercises/16. Bomb Numbers/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _16.Bomb_Numbers
 {
@@ -10,6 +8,25 @@ namespace _16.Bomb_Numbers
     {
         static void Main(string[] args)
         {
+            List<int> numbers =
+                Console.ReadLine().Split(' ')
+                .Select(int.Parse).ToList();
+            int[] bomb = Console.ReadLine().Split(' ')
+                .Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == bomb[0])
+                {
+                    int left = Math.Max(i - bomb[1], 0);
+                    int right = Math.Min(i + bomb[1], numbers.Count - 1);
+                    int lenght = right - left + 1;
+                    numbers.RemoveRange(left, lenght);
+                    i = 0;
+                }
+            }
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
         }
     }
 }
