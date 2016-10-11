8a00fff5dff715ec8e77e6691df95b9ffe1582d9
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-11 12:27:01 +0300

    Objects and Classes - Lab

---
+++ Objects and Classes - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01.Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string date = Console.ReadLine();
+            DateTime dateTime = DateTime
+                .ParseExact(date, "d-M-yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(dateTime.DayOfWeek);
+        }
+    }
+}
---
+++ Objects and Classes - Lab/02. Randomize Words/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ').ToArray();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int tempI = rnd.Next(words.Length);
+                string temp = words[i];
+                words[i] = words[tempI];
+                words[tempI] = temp;
+            }
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
