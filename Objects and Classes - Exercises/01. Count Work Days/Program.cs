845bbee88c0bd807f9b7b3403af5e47071fbb983
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-10-12 09:04:36 +0300

    Objects and Classes - Exercises

---
+++ Objects and Classes - Exercises/01. Count Work Days/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01.Count_Work_Days
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string date = Console.ReadLine();
+            DateTime startDate = DateTime
+                .ParseExact(date, "d-M-yyyy", 
+                CultureInfo.InvariantCulture);
+            
+            date = Console.ReadLine();
+            DateTime endDate = DateTime
+                .ParseExact(date, "d-M-yyyy",
+                CultureInfo.InvariantCulture);
+
+            int totalDays = (int)(endDate - startDate).TotalDays;
+
+            totalDays++;
+
+            for (DateTime i = startDate; i <= endDate; i = i.AddDays(1))
+            {
+                if ((i.Day == 1 && i.Month == 1) ||
+                    (i.Day == 3 && i.Month == 3) ||
+                    (i.Day == 1 && i.Month == 5) ||
+                    (i.Day == 6 && i.Month == 5) ||
+                    (i.Day == 24 && i.Month == 5) ||
+                    (i.Day == 6 && i.Month == 9) ||
+                    (i.Day == 22 && i.Month == 9) ||
+                    (i.Day == 1 && i.Month == 11) ||
+                    ((i.Day == 24 || i.Day == 25 ||
+                    i.Day == 26) && i.Month == 12) ||
+                    (i.DayOfWeek.ToString() == "Saturday") ||
+                    (i.DayOfWeek.ToString() == "Sunday"))
+                {
+                    totalDays--;
+                }
+            }
+            Console.WriteLine(totalDays);
+        }
+    }
+}
